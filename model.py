a75069b7fe0527079dca24368fe9a814dc6eab9f
Gourob Podder <52678361+gkpodder@users.noreply.github.com>
2023-11-08 15:42:33 -0500

    Implemented the feedforward layer with the swiglu activation function

--- model.py
+++ model.py
@@ -168,6 +168,31 @@ class SelfAttention(nn.Module):
         return self.wo(output)
 
 
+class FeedForward(nn.Module):
+
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+
+        hidden_dim = 4 * args.dim
+        hidden_dim = int(2*hidden_dim/3)
+        if args.ffn_dim_multiplier is not None:
+            hidden_dim = int(args.ffn_dim_multiplier * args.dim)
+        # Round the hidden_dim to the nearest multiple of the multiple_of parameter
+        hidden = args.multiple_of * \
+            ((hidden + args.multiple_of - 1) // args.multiple_of)
+
+        self.w1 = nn.Linear(args.dim, hidden_dim, bias=False)
+        self.w2 = nn.Linear(hidden_dim, args.dim, bias=False)
+        self.w3 = nn.Linear(args.dim, hidden_dim, bias=False)
+
+    def forward(self, x: torch.Tensor):
+        swish = F.silu(self.w1(x))
+        x_V = self.w3(x)
+        x = swish * x_V
+        x = self.w2(x)
+        return x
+
+
 class EncoderBlock(nn.Module):
 
     def __init__(self, args: ModelArgs):
